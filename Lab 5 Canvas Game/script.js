fadc3ce07a1e54662f8b88fdcddd72ce1542502f
Shaheim S <smiths1804@student.elms.edu>
2025-10-12 19:20:51 -0400

    Image loaded section, sprite initial locations, Inital state for game added

--- Lab 5 Canvas Game/script.js
+++ Lab 5 Canvas Game/script.js
@@ -1,7 +1,10 @@
 
+
+//Setup
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
+//Image implementation
 const background = new Image();
 const turtleImg = new Image();
 const moleImg = new Image();
@@ -12,3 +15,25 @@ moleImg = Images/Mole.png
 
 const SPRITE_W = 120;
 const SPRITE_H = 90;
+
+//Initial State
+let score = 0;
+const scoreEl = document.getElementById('score');
+const resetBtn = document.getElementById('resetBtn');
+
+//The sprite objects
+const turtle = {x: 100, y: 100, tx: 100, ty: 100, w: SPRITE_W, h: SPRITE_W, Speed: 5, name: 'Turtle'};
+const mole = {x: 600, y: 300, tx: 600, ty: 300, w: SPRITE_W, h: SPRITE_W, Speed: 7, name: 'Mole'};
+
+//Image loading
+let imgsLoaded = 0;
+    [background, turtleImg, moleImg].forEach(img => {
+        img.onload = () => {
+            imgsLoaded++;
+            if(imgsLoaded === 3) startGame();
+        };
+        img.onerror = () => alert('Failed to load ${img.src}'
+        );
+});
+
+
