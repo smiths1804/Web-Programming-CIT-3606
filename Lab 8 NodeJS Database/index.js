86347d8c379968557271d565c93f5d16ac196156
Shaheim S <smiths1804@student.elms.edu>
2025-11-16 12:18:43 -0500

    changes

--- Lab 8 NodeJS Database/index.js
+++ Lab 8 NodeJS Database/index.js
@@ -6,4 +6,18 @@ app.get('/', function(req, res){
 });
 app.listen(8080);
 
+require('dotenv').config();
+const mysql = require('mysql');
+
+const conn = mysql.createConnection({
+  host: "mysql1-p2.ezhostingserver.com",
+  database: "citdemo",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+});
+
+conn.connect((err) => {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
