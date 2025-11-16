715f16d363a5012a1563b4003d1a12af287a1327
Shaheim S <smiths1804@student.elms.edu>
2025-11-16 12:01:06 -0500

    setup configuration 3

--- Lab 8 NodeJS Database/index.js
+++ Lab 8 NodeJS Database/index.js
@@ -1,3 +1,11 @@
+
+const express = require('express'); 
+const app = express();
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+app.listen(8080);
+
 const mysql = require('mysql');
 require('dotenv').config();               
 const conn = mysql.createConnection({
