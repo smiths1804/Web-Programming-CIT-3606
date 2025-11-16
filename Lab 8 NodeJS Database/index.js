43e0f58d6601c73670e15da2b038fd72248f0f67
Shaheim S <smiths1804@student.elms.edu>
2025-11-16 12:34:03 -0500

    test

--- Lab 8 NodeJS Database/index.js
+++ Lab 8 NodeJS Database/index.js
@@ -5,19 +5,3 @@ app.get('/', function(req, res){
    res.send("Hello world!");
 });
 app.listen(8080);
-
-require('dotenv').config();
-const mysql = require('mysql');
-
-const conn = mysql.createConnection({
-  host: "mysql1-p2.ezhostingserver.com",
-  database: "citdemo",
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
-});
-
-conn.connect((err) => {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
