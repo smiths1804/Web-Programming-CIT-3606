4e292f434456860d781934cfb5a7a58cebcbd3f9
Shaheim S <smiths1804@student.elms.edu>
2025-11-16 13:51:44 -0500

    various changes have been implemented

--- Lab 8 NodeJS Database/index.js
+++ Lab 8 NodeJS Database/index.js
@@ -33,3 +33,27 @@ app.post('/Insert', function(req, res){
     );
 })
 
+app.get('/forgot', function(req, res) {
+  res.send(`
+      <form action="/retrieve" method="post">
+          Enter your email: 
+          <input type="text" name="email" maxlength="20"><br>
+          <button type="submit">Retrieve Password</button>
+      </form>
+  `);
+});
+
+app.post('/retrieve', function(req, res) {
+  const sql = "SELECT Username, Password FROM Users WHERE Email = ?";
+  conn.query(sql, [req.body.email], function(err, results) {
+      if (err) throw err;
+
+      if (results.length > 0) {
+          const user = results[0];
+          res.send(`Username: ${user.Username} <br> Password: ${user.Password}`);
+      } else {
+          res.send("Email not found in the database.");
+      }
+  });
+});
+
